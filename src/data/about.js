6db60add4cd8846d5e412e38693d430a2cda7d44
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-02-01 16:04:06 +0700

    update about

--- src/data/about.js
+++ src/data/about.js
@@ -6,17 +6,15 @@ export default {
   facts: [
     "My full name is Hoang Nguyen Duy, but I prefer just $1.",
     "I was born on a wonderful day in 1997.",
-    "I am a citizen of Vietnam, living in its largest city.",
+    "I am a citizen of Vietnam, live in Thai Binh city.",
     "One word to describe my personality: Introversion.",
     "Two words to describe my capability: Good all-rounder.",
     "Three words to describe my living goal: Family and friends.",
   ],
   trivias: [
-    "Baseketball is my favorite sport.",
+    "Foodball is my favorite sport.",
     "I love winter, but only on screen. I can hardly handle the cold in actuality.",
-    "I served the country in a local militia for 2 years.",
-    `There is this video game franchise that has a major impact on my life called
-    Fire Emblem. And I have been serving its $2 as an admin for several years.`,
+    "I love playing games especially League of Legends"
   ],
   links: {
     $1: ["Hoang"],
